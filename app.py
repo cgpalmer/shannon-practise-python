77bf297bc627a883c6ecfb4c3feab9132364aef3
Chris Palmer <cgpalmer91@gmail.com>
2020-09-23 18:32:23 +0000

    Got the amino acid code running.

--- app.py
+++ app.py
@@ -2,38 +2,71 @@ import re
 import json
 
 aminoAcid = """{
-    "AUG": "Methonine"
-    "UUU": 
-    "UUC":
-    "UUA":
-    "UUG":
-    'CUU':
-    'CUC':
-    'CUA':
-    'CUG':
-    'AUU':
-    'AUC':
-    'AUA':
-    'GUU':
-    'GUC':
-    'GUA':
-    'GUG':
-    'UCU':
-    'UCC':
-    'UCA':
-    'UCG':
-    'CCU':
-    'CCC':
-    'CCA':
-    'CCG':
-
+    "GCU":"alanine ",
+    "GCC":"alanine ",
+    "GCA":"alanine ",
+    "GCG":"alanine ",
+    "AGA":"arginine ",
+    "AGG":"arginine ",
+    "CGU":"arginine ",
+    "CGC":"arginine ",
+    "CGA":"arginine ",
+    "CGG":"arginine ",
+    "AAU":"asparagine ",
+    "AAC":"asparagine ",
+    "GAU":"aspartic acid ",
+    "GAC":"aspartic acid ",
+    "UGU":"cysteine ",
+    "UGC":"cysteine ",
+    "CAA":"glutamine ",
+    "CAG":"glutamine ",
+    "GAA":"glutamic acid ",
+    "GAG":"glutamic acid ",
+    "GGU":"glycine ",
+    "GGC":"glycine ",
+    "GGA":"glycine ",
+    "GGG":"glycine ",
+    "CAU":"histidine ",
+    "CAC":"histidine ",
+    "AUU":"isoleucine ",
+    "AUC":"isoleucine ",
+    "AUA":"isoleucine ",
+    "UUA":"leucine ",
+    "UUG":"leucine ",
+    "CUU":"leucine ",
+    "CUC":"leucine ",
+    "CUA":"leucine ",
+    "CUG":"leucine ",
+    "AAA":"lysine ",
+    "AAG":"lysine ",
+    "AUG":"methionine ",
+    "UUU":"phenylalanine ",
+    "UUC":"phenylalanine ",
+    "CCU":"proline ",
+    "CCC":"proline ",
+    "CCA":"proline ",
+    "CCG":"proline ",
+    "AGU":"serine ",
+    "AGC":"serine ",
+    "UCU":"serine ",
+    "UCC":"serine ",
+    "UCA":"serine ",
+    "UCG":"serine ",
+    "ACU":"threonine ",
+    "ACC":"threonine ",
+    "ACA":"threonine ",
+    "ACG":"threonine ",
+    "UGG":"tryptophan ",
+    "UAU":"tyrosine ",
+    "UAC":"tyrosine ",
+    "GUU":"valine ",
+    "GUC":"valine ",
+    "GUA":"valine ",
+    "GUG":"valine ",
+    "UAA":"STOP ",
+    "UAG":"STOP ",
+    "UGA":"STOP "
 }"""
-
-
-
-
-
-
 
 aminoSearch = json.loads(aminoAcid)
 
@@ -43,42 +76,17 @@ def fasterFindCodons(rna):
     return(new_method)
 
 
-rna = 'augcgcugaagucagugucaggac'
-stopCodon = ['UAA', 'UAG', 'UGA']
+rna = input("Paste in your RNA here:")
 
 codons = fasterFindCodons(rna)
+aminoAcidList = []
+
 
 def matchAminoAcids(codons):
     for codon in codons:
         if codon in aminoSearch:
-            print("success")
-            print(aminoSearch[codon])
-        else:
-            print("Not in database...yet")
+            aminoAcidList.append(aminoSearch[codon])
+    print(aminoAcidList)
+
 
 matchAminoAcids(codons)
-'''
-Find all the codons
-
-First function
-is first codon !aug = error in sequence
-else remove from sequence and continue
-
-Second function
-Loop through codons
-    if codon = stop codon then aminoAcid = STOP
-
-Third Function
-if first letter  = a g u c then give respective value 
-return first letter
-
-Forth Function
-if the second letter is 
-
-'''
-
-
-
-
-
-
